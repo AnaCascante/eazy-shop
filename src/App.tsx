fe6f590ee62e9b901b6c59db5040240c83ed0799
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-06-05 22:53:51 +0200

    adds components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,66 @@
-import { useState } from 'react'
+
+import Header from '../Header';
+import Logo from '../Logo';
+import NavBar from '../NavBar';
+import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from '../../pages/Homepage';
+
+import ProductPage from '../../pages/Productpage'; 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
+  {
+    path: "/",
+    element: <Logo />,
+  },
+  {
+    path: "/",
+    element: <NavBar />,
+  },
+  {
+    path: "/",
+    element: <ProductPage />, 
+  }
+]);
+
+function App() {
+  return (
+   <RouterProvider router ={router}>     
+    <div>
+      <Header />
+      <Logo />
+      <NavBar />  
+    </div>
+    </RouterProvider>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,28 +70,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      hello world
     </>
   )
 }
 
-export default App
+export default App */
---
+++ src/components/logo/index.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+    return (
+      <div className="logo">
+        <img src="/src/images/Eazy_logo.png" alt="logo" />
+      </div>
+    );
+  }
+  
+  export default Logo;
