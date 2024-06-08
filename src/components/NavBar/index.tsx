5ba319dda4934ce88e9ad0f4a36d388a0143b0a5
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-06-08 13:21:30 +0200

    carticon missing product count

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,14 +4,16 @@ import Logo from "../Logo";
 
 
 
+
 function NavBar() {
   return (
     <nav className="flex flex-row bg-slate-200 justify-normal ">
         <Logo/>
          <NavLink to="/" className="hover:text-pink-500 m-3 ">Home</NavLink>
-         <NavLink to="/ContactPage" className="hover:text-pink-500 m-3">Contact</NavLink> 
-         <NavLink to="/CheckoutPage" className="hover:text-pink-500 m-3"><FaShoppingCart /></NavLink>
-         
+         <NavLink to="/ContactPage" className="hover:text-pink-500 m-3">Contact</NavLink>
+         <div className=" m-0">
+         <NavLink to="/CheckoutPage" className="hover:text-pink-500 m-0.5"><FaShoppingCart /></NavLink>
+         </div> 
     </nav>
   );
 }
